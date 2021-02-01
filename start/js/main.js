b9c5a7766ad1f97660ea7e311a0f1f4c5a10c09d
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-01 14:30:54 +0700

    Update

--- start/js/main.js
+++ start/js/main.js
@@ -6,7 +6,10 @@ $(".animsition").animsition({
   outDuration: 500
 });
 
-$('.header').sticky();
+$('.header').sticky({
+  getWidthFrom: '.container',
+  responsiveWidth: true
+});
 
 $('.header').on('sticky-start', function () {
   $('.description').html('We build <strong>great</strong> apps');
@@ -14,4 +17,18 @@ $('.header').on('sticky-start', function () {
 
 $('.header').on('sticky-end', function () {
   $('.description').html('We build apps');
+});
+
+$('.work').sticky({
+  topSpacing: 64,
+  getWidthFrom: '.container',
+  responsiveWidth: true
+});
+
+$('.work').on('sticky-start', function () {
+  $(this).append(' <a href="mailto:example@gmail.com" class="email-text">Email us</a>');
+});
+
+$('.work').on('sticky-end', function () {
+  $('.email-text').remove();
 });
